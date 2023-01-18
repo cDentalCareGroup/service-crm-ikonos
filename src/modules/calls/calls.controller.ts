7e3d5d9897b2428586b987a0e896534111c803c9
Immanuel Diaz <immanuel.diaz@immanuel.diaz-pro>
2023-01-18 08:19:27 -0600

    setup calls

--- src/modules/calls/calls.controller.ts
+++ src/modules/calls/calls.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
 import { CallsService } from './calls.service';
+import { RegisterCatalogDTO, UpdateCallDTO, UpdateCatalogDTO } from './models/call.dto';
 
 @Controller('calls')
 export class CallsController {
@@ -12,5 +14,28 @@ export class CallsController {
     @Get('/')
     async getCalls() {
         return this.callsService.getCalls();
+    }
+
+    @Get('/catalogs')
+    async getCatalogs() {
+        return this.callsService.getCatalos();
+    }
+
+    @Post('/update')
+    @ApiBody({type: UpdateCallDTO})
+    async updateCall(@Body() body: UpdateCallDTO) {
+        return this.callsService.updateCall(body);
+    }
+
+    @Post('/update/catalog')
+    @ApiBody({type: UpdateCallDTO})
+    async updateCatalog(@Body() body: UpdateCatalogDTO) {
+        return this.callsService.updateCatalog(body);
+    }
+
+    @Post('/register/catalog')
+    @ApiBody({type: RegisterCatalogDTO})
+    async registerCatalog(@Body() body: RegisterCatalogDTO) {
+        return this.callsService.registerCatalog(body);
     }
 }
--- src/modules/calls/calls.service.ts
+++ src/modules/calls/calls.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { async } from 'rxjs';
 import { HandleException } from 'src/common/exceptions/general.exception';
 import { Repository } from 'typeorm';
 import { AppointmentEntity } from '../appointment/models/appointment.entity';
 import { PatientEntity } from '../patient/models/patient.entity';
 import { CallCatalogEntity } from './models/call.catalog.entity';
+import { GetCallsDTO, RegisterCatalogDTO, UpdateCallDTO, UpdateCatalogDTO } from './models/call.dto';
 import { CallEntity } from './models/call.entity';
 
 @Injectable()
@@ -17,40 +19,70 @@ export class CallsService {
         @InjectRepository(AppointmentEntity) private appointmentRepository: Repository<AppointmentEntity>,
         @InjectRepository(CallCatalogEntity) private catalogRepository: Repository<CallCatalogEntity>,
 
-    ) {
-
-    }
-
+    ) { }
 
     getCalls = async () => {
         try {
-            const result = await this.callRepository.find();
-            let data: GetCalls[] = [];
+            const result = await this.callRepository.findBy({ status: 'activa' });
+            let data: GetCallsDTO[] = [];
             for await (const call of result) {
                 const patient = await this.patientRepository.findOneBy({ id: call.patientId });
                 const appintment = await this.appointmentRepository.findOneBy({ id: call.appointmentId });
                 const catalog = await this.catalogRepository.findOneBy({ id: call.caltalogId });
-                data.push(new GetCalls(call, patient, catalog, appintment));
+                data.push(new GetCallsDTO(call, patient, catalog, appintment));
             }
             return data;
         } catch (error) {
             HandleException.exception(error);
         }
     }
-}
 
+    getCatalos = async () => {
+        try {
+            const result = await this.catalogRepository.find();
+            return result;
+        } catch (error) {
+            HandleException.exception(error);
+        }
+    }
 
-class GetCalls {
-    call: CallEntity;
-    patient: PatientEntity;
-    catalog: CallCatalogEntity;
-    appointment?: AppointmentEntity;
-    constructor(call: CallEntity,
-        patient: PatientEntity,
-        catalog: CallCatalogEntity, appointment?: AppointmentEntity,) {
-        this.call = call;
-        this.patient = patient;
-        this.appointment = appointment;
-        this.catalog = catalog;
+    updateCall = async ({ id, description }: UpdateCallDTO) => {
+        try {
+            const result = await this.callRepository.findOneBy({ id: id });
+            result.status = 'resuelta';
+            result.description = description;
+            result.effectiveDate = new Date();
+            result.comments = `LLamada resuelta por call center - ${new Date()}`;
+            return await this.callRepository.save(result);
+        } catch (error) {
+            HandleException.exception(error);
+        }
+    }
+
+    updateCatalog = async ({ id, description, goal, script, name }: UpdateCatalogDTO) => {
+        try {
+            const catalog = await this.catalogRepository.findOneBy({ id: id });
+            catalog.name = name;
+            catalog.description = description;
+            catalog.goal = goal;
+            catalog.script = script;
+            return await this.catalogRepository.save(catalog);
+        } catch (error) {
+            HandleException.exception(error);
+        }
+    }
+
+    registerCatalog = async ({ name, description, goal, script }: RegisterCatalogDTO) => {
+        try {
+            const catalog = new CallCatalogEntity();
+            catalog.name = name;
+            catalog.description = description;
+            catalog.goal = goal;
+            catalog.script = script;
+            return await this.catalogRepository.save(catalog);
+        } catch (error) {
+            HandleException.exception(error);
+        }
     }
 }
+
