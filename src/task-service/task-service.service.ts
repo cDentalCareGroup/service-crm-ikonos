f4c12bbc77036992a6b540995ba4a12ef8b8a72d
Immanuel Diaz <immanuel.diaz@immanuel.diaz-pro>
2023-07-18 17:50:01 -0600

    change task service

--- src/task-service/task-service.service.ts
+++ src/task-service/task-service.service.ts
@@ -9,26 +9,26 @@ export class TaskServiceService {
 
   constructor(private appointmentService: AppointmentService) { }
 
-  @Cron('0 30 11 * * 0-6')
-  async handleRemiderAppointment() {
-    const date = new Date();
-    date.setDate(date.getDate() + 1);
-    const nextDate = date.toISOString().split("T")[0];
-    console.log(`Executing job handleRemiderAppointmentat ${nextDate}`);
-    const data = await this.appointmentService.appointmentReminders();
-    console.log(`Whatsapps ${data}`);
-  }
+  // @Cron('0 30 11 * * 0-6')
+  // async handleRemiderAppointment() {
+  //   const date = new Date();
+  //   date.setDate(date.getDate() + 1);
+  //   const nextDate = date.toISOString().split("T")[0];
+  //   console.log(`Executing job handleRemiderAppointmentat ${nextDate}`);
+  //   const data = await this.appointmentService.appointmentReminders();
+  //   console.log(`Whatsapps ${data}`);
+  // }
 
-  @Cron('0 30 23 * * 0-6')
-  async handleRemiderNotAttendedAppointment() {
-    const date = new Date();
-    date.setDate(date.getDate() - 1);
-    const nextDate = date.toISOString().split("T")[0];
-    console.log(`Executing job handleRemiderNotAttendedAppointment at ${nextDate}`);
-    const data = await this.appointmentService.appointmentNotAttended();
-    console.log(`Generated calls -  ${JSON.stringify(data)}`);
-    console.log(`Generated calls -  ${data}`);
-  }
+  // @Cron('0 30 23 * * 0-6')
+  // async handleRemiderNotAttendedAppointment() {
+  //   const date = new Date();
+  //   date.setDate(date.getDate() - 1);
+  //   const nextDate = date.toISOString().split("T")[0];
+  //   console.log(`Executing job handleRemiderNotAttendedAppointment at ${nextDate}`);
+  //   const data = await this.appointmentService.appointmentNotAttended();
+  //   console.log(`Generated calls -  ${JSON.stringify(data)}`);
+  //   console.log(`Generated calls -  ${data}`);
+  // }
 
 
   @Cron('0 30 9 * * 0-6')
