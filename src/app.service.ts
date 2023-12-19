52330559321e75dd462fd7174d176bbe8e8a5457
Immanuel Diaz <immanuel.diaz@immanuel.diaz-pro>
2023-12-19 10:22:13 -0600

    update package versions

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,28 +1,12 @@
-import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { addMinutes, subMinutes } from 'date-fns';
-import { formatInTimeZone } from 'date-fns-tz';
-import { lastValueFrom, map } from 'rxjs';
-import { Repository } from 'typeorm';
-import { HandleException } from './common/exceptions/general.exception';
-import { UserLogsEntity } from './modules/auth/models/entities/user.logs.entity';
-import { capitalizeAllCharacters, formatDateToWhatsapp, getSimpleTodayDate, getTodayDate, getTodaySimpleDate } from './utils/general.functions.utils';
 
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly httpService: HttpService,
-  ) {
-
-  }
+  c
   async getHello() {
     return {
       'version': `Version 1.0.49.5-${process.env.ENV_NAME}`,
-      'today Date': getTodayDate(),
-      'simple Today Date': getSimpleTodayDate(),
-      'today Simple Date': getTodaySimpleDate(),
       'variables': {
         'DNAME': process.env.DB_NAME,
         'WS': process.env.WTS_API_URL,
