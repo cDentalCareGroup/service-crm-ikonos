55c6fef65178a914fff686f412868ac9932f5494
ThayliAreliVillegas112 <20203tn064@utez.edu.mx>
2024-01-17 18:44:47 -0600

    update service to get patients by status

--- src/modules/patient/patient.controller.ts
+++ src/modules/patient/patient.controller.ts
@@ -75,5 +75,11 @@ export class PatientController {
     async updateLatLng (@Body() body: UpdateLatLngDTO) {
         return this.patientService.updateLatLng(body);
     }
+    // Nueva ruta para obtener pacientes por estado
+    @Post('getPatient/filter-by-status')
+    async getPatientsByStatus(@Body() body: { status: string }): Promise<PatientEntity[]> {
+        const { status } = body;
+        return this.patientService.getPatientsByStatus(status);
+    }
 
 }
