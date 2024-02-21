917124b1df5a865433be7fea9bbd2a22bf71705e
ThayliAreliVillegas112 <20203tn064@utez.edu.mx>
2024-02-21 14:47:07 -0600

    remove commented code

--- src/modules/appointment/models/appointment.entity.ts
+++ src/modules/appointment/models/appointment.entity.ts
@@ -61,12 +61,6 @@ export class AppointmentEntity {
   })
   status: string;
 
-  // @Column({
-  //   name: 'cost_amount',
-  //   type: 'decimal',
-  //   nullable: true,
-  // })
-  // costAmount: number;
 
   @Column({
     name: 'price_amount',
@@ -96,20 +90,6 @@ export class AppointmentEntity {
     nullable: true,
   })
   scheduledAt: string;
-
-  // @Column({
-  //   name: 'treatment_category_id',
-  //   type: 'int',
-  //   nullable: true,
-  // })
-  // treatmentCategoryId: number;
-
-  // @Column({
-  //   name: 'treatment_category',
-  //   type: 'varchar',
-  //   nullable: true,
-  // })
-  // treatmentCategory: string;
 
   @Column({
     name: 'folio',
@@ -153,14 +133,6 @@ export class AppointmentEntity {
     nullable: true,
   })
   hasCabinet: number;
-
-
-  // @Column({
-  //   name: 'payment_method_id',
-  //   type: 'int'
-  // })
-  // paymentMethodId: number;
-
 
 
   @Column({
@@ -208,106 +180,6 @@ export class AppointmentEntity {
   call?: CallEntity;
   referralName?: string;
   referralId?: number;
-
-
-  // @Column({
-  //   name: 'dentist_comments',
-  //   type: 'text',
-  // })
-  // dentistComments: string;
-
-  // @Column({
-  //   name: 'receptionist_comments',
-  //   type: 'text',
-  // })
-  // receptionistComments: string;
-
-
-  //   @Column({
-  //     name: 'dentist_id',
-  //     type: 'int',
-  //     comment: 'id del dentista que atendera la cita',
-  //   })
-  //   dentistID: number;
-
-  //   @Column({
-  //     name: 'receptionist_id',
-  //     type: 'int',
-  //     comment: 'id de la recepcionista que recibio al paciente',
-  //   })
-  //   receptionistID: number;
-
-  //   @Column({
-  //     name: 'datetime_appointment', 
-  //     type: 'datetime',
-  //     comment: 'fecha hora de la cita',
-  //   })
-  //   dateTimeAppointdment: Date;
-
-  //   @Column({
-  //     name: 'appointment_type',
-  //     type: 'varchar',
-  //     length: 45,
-  //     comment: 'tipo de cita: seguimiento, cita nueva, reposicion (se cancelo una anterior)',
-  //   })
-  //   appointmentType: string;
-
-  //   @Column({ 
-  //     name: 'appointment_reason', 
-  //     nullable: true })
-  //   appointmentReason: string;
-
-  //   @Column({ name: 'import_service', nullable: true })
-  //   importService: number;
-
-  //   @Column({ name: 'payment_method_service', nullable: true })
-  //   paymentMethodService: string;
-
-  //   @Column({ name: 'import_product', nullable: true })
-  //   importProduct: number;
-
-  //   @Column({ name: 'payment_method_product', nullable: true })
-  //   paymentMethodProduct: string;
-
-  //   @Column({ 
-  //     name: 'status',
-  //     type: "varchar",
-  //     length: 10,
-  //     nullable: false, 
-  //   })
-  //   status: string;
-
-  //   @Column({
-  //     name: 'comments', 
-  //     type: 'varchar',
-  //     nullable: true,
-  //   })
-  //   comments: string;
-
-  //   @Column({
-  //     name: 'created_at',
-  //     type: 'timestamp',
-  //     default: () => 'CURRENT_TIMESTAMP',
-  //     nullable: false,
-  //   })
-  //   dateTime: Date;
-
-
-  //   // @OneToMany(() => BranchOffice)
-  //   // @JoinColumn({ name: 'branch_office_id' })
-  //   // branchOffice: BranchOffice;
-
-  //   // @OneToOne(() => Patient)
-  //   // @JoinColumn({ name: 'client_id' })
-  //   // patient: Patient;
-
-  //   // @OneToOne(() => Employees)
-  //   // @JoinColumn({ name: 'dentist_id' })
-  //   // dentist: Employees;
-
-  //   // @OneToOne(() => Employees)
-  //   // @JoinColumn({ name: 'receptionist_id' })
-  //   // receptionist: Employees;
 
   @ManyToOne(type => EmployeeEntity, dentist => dentist.id)
   @JoinColumn({ name: 'dentist_id' })
